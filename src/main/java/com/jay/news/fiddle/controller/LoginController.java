674543aaa57e12a9af7d7480cf6c99004493da86
jayandh06 <r_jayandh@hotmail.com>
2014-02-28 16:26:19 -0600

    Change structure

--- src/main/java/com/jay/news/fiddle/controller/LoginController.java
+++ src/main/java/com/jay/news/fiddle/controller/LoginController.java
@@ -1,16 +1,30 @@
 package com.jay.news.fiddle.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jay.news.fiddle.service.UserService;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+	@Autowired
+	UserService userService;
+	
 	@RequestMapping("/validate")
 	@ResponseBody
 	public boolean validateLogin(){
+		
 		return true;
+	}
+	
+	@RequestMapping("/signup.htm")
+	public ModelAndView newUserSignup(){
+		ModelAndView model = new ModelAndView("signup");
+		return model;
 	}
 }
