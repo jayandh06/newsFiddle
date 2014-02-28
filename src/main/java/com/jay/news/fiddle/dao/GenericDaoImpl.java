16d8e45d450eccc00befbecc3a61e565070192e0
jayandh06 <r_jayandh@hotmail.com>
2014-02-28 07:08:24 -0600

    Modified getAll query in GenericDao, Added log4j.properties

--- src/main/java/com/jay/news/fiddle/dao/GenericDaoImpl.java
+++ src/main/java/com/jay/news/fiddle/dao/GenericDaoImpl.java
@@ -1,18 +1,15 @@
 package com.jay.news.fiddle.dao;
 
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-public class GenericDaoImpl<T> implements GenericDao<T> {
+public abstract class GenericDaoImpl<T> implements GenericDao<T> {
 
 	@PersistenceContext
 	protected EntityManager em;
@@ -20,9 +17,10 @@ public class GenericDaoImpl<T> implements GenericDao<T> {
 	private Class<T> type;
 
 	public GenericDaoImpl() {
-		Type t = getClass().getGenericSuperclass();
-		ParameterizedType pt = (ParameterizedType) t;
-		type = (Class) pt.getActualTypeArguments()[0];
+
+		type = (Class<T>) ((ParameterizedType) getClass()
+				.getGenericSuperclass()).getActualTypeArguments()[0];
+		;
 	}
 
 	public EntityManager getEntityManager() {
@@ -42,7 +40,7 @@ public class GenericDaoImpl<T> implements GenericDao<T> {
 
 	@Transactional
 	public List<T> getAll() {
-		final StringBuffer queryString = new StringBuffer("SELECT * from ");
+		final StringBuffer queryString = new StringBuffer("SELECT o from ");
 
 		queryString.append(type.getSimpleName()).append(" o ");
 
