c80e2cbd59f0031fced2560729aecf8823936310
jayandh06 <r_jayandh@hotmail.com>
2014-03-21 07:49:59 -0500

    Added seperate controller for Country

---
+++ src/main/java/com/jay/news/fiddle/controller/CountryController.java
@@ -0,0 +1,29 @@
+package com.jay.news.fiddle.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.jay.news.fiddle.domain.Country;
+import com.jay.news.fiddle.service.CountryService;
+
+@Controller
+@RequestMapping("/country")
+public class CountryController {
+
+	@Autowired
+	CountryService countryService;
+	
+	@RequestMapping(value="/list",method=RequestMethod.GET)
+	@ResponseBody
+	public String getAllCountries() {
+		List<Country> countries = countryService.getAllCountries();
+		Gson gson = new Gson();
+		return gson.toJson(countries);
+	}
+}
