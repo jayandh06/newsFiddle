96893b5e808e3e4b770064f16261f64b642877ce
jayandh06 <r_jayandh@hotmail.com>
2014-03-26 07:46:09 -0500

    Update search feed

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -5,6 +5,13 @@ com.company.rssfiddle.js.header = {
 			var keyCode = event.keyCode || event.which;
 			if(keyCode == 13){
 				console.log("enter key pressed");
+				var query = $(this).val();
+				if(query.length >= 4){
+					google.feeds.findFeeds(query, headerObj.findDone);
+				}
+				else{
+					console.log('Search text very small');
+				}
 			}
 		}).on('click',function(){
 			if($(this).val() == "Search feed") {
@@ -12,7 +19,25 @@ com.company.rssfiddle.js.header = {
 			}
 		});
 		
-	}
+	},
+	findDone: function (result) {
+		  $("#feeds-container").empty();
+		  // Make sure we didn't get an error.
+		  if (!result.error) {
+		    // Get content div
+		    var content = document.getElementById('feeds-container');
+		    var html = '';
+
+		    // Loop through the results and print out the title of the feed and link to
+		    // the url.
+		    for (var i = 0; i < result.entries.length; i++) {
+		      var entry = result.entries[i];
+		      html += '<p>'+entry.contentSnippet+'<br><a href="' + entry.url + '">' + entry.title + '</a></p><hr>';
+		    }
+		    content.innerHTML = html;
+		  }
+		}
+	
 
 };
 var headerObj = com.company.rssfiddle.js.header;
